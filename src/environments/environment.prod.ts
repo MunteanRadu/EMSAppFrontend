fc49f604ab83f986463e8171cd97978c1aad04ec
Radu Muntean <radumuntean03@gmail.com>
2025-06-15 02:08:36 +0300

    modified backend api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://emsapp-euffdyd3dwhgdzav.polandcentral-01.azurewebsites.net'
+  apiUrl: 'https://emsapp-euffdyd3dwhgdzav.polandcentral-01.azurewebsites.net/api'
 };
